39eca3a6fc2d7897bc5ec029387e51bb6b53f516
Stephanie Perea <AFYAWellth@gmail.com>
2021-05-16 23:42:00 -0600

    replaced code with correct step motor code

--- StepMotor_Test/src/StepMotor_Test.cpp
+++ StepMotor_Test/src/StepMotor_Test.cpp
@@ -3,35 +3,42 @@
 /******************************************************/
 
 #include "Particle.h"
-#line 1 "c:/Users/kareem/Documents/IOT/Smart-Farming-System/StepMotor_Test/src/StepMotor_Test.ino"
+#line 1 "c:/Users/AFYAW/OneDrive/Documents/IOT/Smart-Farming-System/StepMotor_Test/src/StepMotor_Test.ino"
 /*
- * Project L16_02_AutoRotate
- * Description: display date and time on an OLED display and use accel values to ato-rotate the OLED
+ * Project L16_05_StepperGyro
+ * Description: Wire stepper motor and move motor 2 rotations clockwise, pause, move 1 rotation counter-clockwise, repeat
  * Author:Stephanie Perea
- * Date: May 13, 2021
- */
+ * Date:05/16/2021
+*/
 
 #include <Wire.h>
 #include <math.h>
-#include "Adafruit_SSD1306.h"
-#include "Adafruit_GFX.h"
-void setup();
-void loop();
-#line 12 "c:/Users/kareem/Documents/IOT/Smart-Farming-System/StepMotor_Test/src/StepMotor_Test.ino"
-#define OLED_RESET D4
-#define SCREEN_ADDRESS 0x3C
-#define XPOS 0
-#define YPOS 1
-#define OLED_RESET D4
-Adafruit_SSD1306 display(OLED_RESET);
+#include <Stepper.h>
+
 
  // Declare variables
+void setup();
+void loop();
+#line 14 "c:/Users/AFYAW/OneDrive/Documents/IOT/Smart-Farming-System/StepMotor_Test/src/StepMotor_Test.ino"
 byte accel_x_h,accel_x_l; // variables to store the individual btyes
 byte accel_y_h,accel_y_l; // variables to store the individual bytes
 byte accel_z_h,accel_z_l; // variables to store the individual bytes
+
 int16_t accel_x; // variable to store the x- acceleration
 int16_t accel_y; // variable to store the y- acceleration
 int16_t accel_z; // variable to store the z- acceleration
+
+//int16_t myStepper;
+int16_t stepper_in1 = D1; //variable to store info from IN1
+int16_t stepper_in2 = D2; //variable to store info from IN2
+int16_t stepper_in3 = D3; //variable to store info from IN3
+int16_t stepper_in4 = D4; //variable to store info from IN4 
+int16_t stepperSpeed=15;
+int16_t stepperSteps;
+int16_t spr=2048;
+Stepper myStepper (2048, D1,D3, D2, D4);
+
+
 float ax=-16236.0;
 float ay=15648.0;
 float az=15612.0;
@@ -41,19 +48,11 @@ float SFz=-1/az;
 float axg;
 float ayg;
 float azg;
-const char degree=0xA7;
-int rot=1-3;
-
+float rpm = 1-15;
+float speed=rpm;
+SYSTEM_MODE(SEMI_AUTOMATIC);
 // setup() runs once, when the device is first turned on.
 void setup() {
-  Serial.begin(9600);
-
-    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
-  
-  while(!Serial);    // time to get serial running
-
-  //unsigned int status;
-
   //Begin I2C communications
   Wire.begin();
 
@@ -68,9 +67,7 @@ Wire.write (0) ; // set to 0 ( wakes up MPU -6050)
 Wire.endTransmission (true) ;
 }
 
-
 // loop() runs over and over again, as quickly as it can execute.
-
 void loop() {
   // The core of your code will likely live here.
 // Set the " pointer " to the 0x3B memory location of the MPU and wait for data
@@ -107,15 +104,11 @@ delay(3000);
 Serial.printf ("Z- azg %f \n", azg);
 delay(3000);
 
-display.begin(SSD1306_SWITCHCAPVCC, 0x3D);  // initialize with the I2C addr 0x3D (for the 128x64)
-  // init done
-  if (axg>.8) {
-    display.setRotation (0);
-  }  
-    if (axg<.8){
-      display.setRotation (2);
-    }
-    display.display(); // show splashscreen
-  delay(2000);
-  display.clearDisplay();   // clears the screen and buffer
+myStepper.setSpeed(15);
+myStepper.step(4096);
+delay(9000);
+myStepper.step(-2048);
+delay(5000);
+ 
+
 }
